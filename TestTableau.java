a35b72659d9501a52015b8de6ae3fae30ca25e2b
Bilel Majdoub <bilelmajdoub@air-de-bilel.home>
2022-09-18 19:20:56 +0200

    toString() avec U

--- TestTableau.java
+++ TestTableau.java
@@ -1,5 +1,8 @@
 public class TestTableau{
 	public static void main(String[] args){
 		TableauInt T = new TableauInt();
+		TableauInt U = new TableauInt(20);
+
+		System.out.println(U.toString());
 	}
 }
