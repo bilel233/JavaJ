96271670df29aff7d112583d0ad38fee563400a0
Bilel Majdoub <bilelmajdoub@air-de-bilel.home>
2022-09-18 18:54:36 +0200

    second constructeur

--- TableauInt.java
+++ TableauInt.java
@@ -6,5 +6,12 @@ public class TableauInt{
 		for(int i=0;i<tab.length;i++){
 			tab[i]=(int)Math.random()%101;
 		}
+
+	}
+	public TableauInt(int n){
+		tab = new int[n]; // le tableau à n cases
+		for(int i=n;i<tab.length;i++){
+			tab[i] = n+i;
+		}
 	}
 }
