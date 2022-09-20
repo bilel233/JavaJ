0d55ec764c8d4c0533d46ee119d041c2674cb2fd
Bilel Majdoub <bilelmajdoub@MacBook-Air-de-Bilel.local>
2022-09-20 12:52:14 +0200

    class Personne

---
+++ Personne.java
@@ -0,0 +1,23 @@
+public class Personne{
+    protected final String nom;
+    protected String numTel;
+    private int nbEnfants;
+
+    public Personne(String nom,String numTel){
+        this.nom=nom;
+        this.numTel=numTel;
+        nbEnfants=0;
+    }
+    public String getNom(){
+        return nom;
+    }
+    public String numTel(){
+        return numTel;
+    }
+    protected int getNbEnfants(){
+        return nbEnfants;
+    }
+    public void ajouterEnfant(){
+        nbEnfants++;
+    }
+}
