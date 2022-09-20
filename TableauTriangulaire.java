6e0db326d598cecc47928047edbca790c0c4a9c7
Bilel Majdoub <bilelmajdoub@MacBook-Air-de-Bilel.local>
2022-09-20 12:38:15 +0200

    class TableauTriangulaire

---
+++ TableauTriangulaire.java
@@ -0,0 +1,12 @@
+public class TableauTriangulaire{
+    public static void main(String[] args){
+        int[][] tab = new int[3][];
+        for(int i = 0;i<tab.length;i++){
+            tab[i] = new int[i+1];
+        }
+    
+    }
+    
+
+
+}
