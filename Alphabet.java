c41ca87954d617a20ca51eeaa33e481dbbb5ebaa
Bilel Majdoub <bilelmajdoub@air-de-bilel.home>
2022-09-26 10:48:59 +0200

    code ASCII

--- Alphabet.java
+++ Alphabet.java
@@ -1,9 +1,21 @@
 public class Alphabet {
     public static void main(String[] args){
+       
         System.out.println("affichage des chiffres de 0 a 9");
         System.out.println("*=====================================================*");
         for(int i=0;i<=9;i++){
             System.out.println(i);
+            System.out.println("--------------------------------");
+            
+            
+        }
+        System.out.println("*=====================================================*");
+        for(char c='0';c<'9';c++){
+            int a;
+            a = (int)c;
+            System.out.println(a);
+
+
         }
 
     }
