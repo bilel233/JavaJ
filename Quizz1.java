ed98336b6edfa1dc6fbfa07bbc0bbf9dbd403a79
Bilel Majdoub <bilelmajdoub@air-de-bilel.home>
2022-09-26 11:28:31 +0200

    bool et char

--- Quizz1.java
+++ Quizz1.java
@@ -4,6 +4,10 @@ public class Quizz1{
         System.out.println(val);
         int res = (int)(Math.random()*(101)+50);
         System.out.println(res);
+        boolean b = Math.random() < 0.25;
+        System.out.println(b);
+        char c = (char)(Math.random()*('z'-'a')+'a');
+        System.out.println(c);
 
     }
 }
