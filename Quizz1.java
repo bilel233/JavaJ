140dd894d23b528b4b3c6c73cac1ab9bb3ed268c
Bilel Majdoub <bilelmajdoub@air-de-bilel.home>
2022-09-26 11:22:48 +0200

    valeur aleatoire double et int

---
+++ Quizz1.java
@@ -0,0 +1,9 @@
+public class Quizz1{
+    public static void main(String[] args){
+        double val = Math.random()*20 + 10;
+        System.out.println(val);
+        int res = (int)(Math.random()*(101)+50);
+        System.out.println(res);
+
+    }
+}
